6a89e8738eb646333ddc6103f8592cd848555db9
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-07-20 13:48:36 -0500

    inner join authors and articles set up

--- starter-code/server.js
+++ starter-code/server.js
@@ -23,8 +23,8 @@ app.get('/new', function(request, response) {
 
 app.get('/articles', function(request, response) {
   // REVIEW: This query will join the data together from our tables and send it back to the client.
-  // TODO: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
-  client.query(``)
+  // DONE: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
+  client.query('SELECT ar.*, au.author, au."authorUrl" FROM articles ar JOIN authors au ON ar.author_id = au.author_id;')
   .then(function(result) {
     response.send(result.rows);
   })
